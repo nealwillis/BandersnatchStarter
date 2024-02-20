69bbb4f4dcc374f5254557e5357fe1337b0419f2
Neal Willis <nealwillis@gmail.com>
2024-02-20 16:07:39 -0500

    Added seed(), reset(), count(), dataframe(), and html_table() functions to interact with MongoDB

--- app/data.py
+++ app/data.py
@@ -1,25 +1,47 @@
 from os import getenv
-
+import pandas as pd
 from certifi import where
 from dotenv import load_dotenv
 from MonsterLab import Monster
-from pandas import DataFrame
 from pymongo import MongoClient
+from pymongo.mongo_client import MongoClient
+
+load_dotenv()
+
+# Create a new client and connect to the server
+client = MongoClient(getenv("DB_URL"), tlsCAFile=where())['Database']
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+collection = client["Monsters"]
+
 
 
 class Database:
-
     def seed(self, amount):
-        pass
+        """Inserts a new monster into the Monster collection based on input"""
+        monsters = []
+        monster_dict_list = []
+        for i in range(amount):
+            monsters.append(Monster().to_dict())
+        uid = collection.insert_many(monsters)
 
     def reset(self):
-        pass
+        """Removes all monsters from the Monster collection"""
+        collection.delete_many({})
 
     def count(self) -> int:
-        pass
+        """Returns the number of monsters in the collection"""
+        return collection.count_documents({})
 
-    def dataframe(self) -> DataFrame:
-        pass
+    def dataframe(self) -> pd.DataFrame:
+        """Turns the monster collection into a pandas dataframe"""
+        return pd.DataFrame(list(collection["Monsters"].find({})))
 
     def html_table(self) -> str:
-        pass
+        """Turns monster collection into a html table"""
+        return str(pd.DataFrame(list(collection.find({}))).drop(columns="_id").to_html())
